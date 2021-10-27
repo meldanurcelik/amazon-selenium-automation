245bd77321151f7097f8d05cbc4030dcbad9d395
meldanurcelik <melda_celik@hotmail.com>
2021-10-27 20:33:37 +0300

    BaseTest classı oluşturuldu. Test_Add_Product_To_Card classının Assertions'ları yazıldı.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://www.amazon.com.tr/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterAll
+    public void tearDown(){
+        driver.quit();
+    }
 }
--- src/test/java/Test_Add_Product_To_Card.java
+++ src/test/java/Test_Add_Product_To_Card.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Test_Add_Product_To_Card {
+
+    HomePage homePage;
+    ProductsPage productsPage;
+    ProductDetailPage productDetailPage;
+    CartPage cartPage;
+
+    @Test
+    public void search_a_product(){
+        homePage.searchBox().search("Laptop");
+        Assertions.assertTrue(productsPage.isOnProductPage(), "Not on products page!");
+    }
+
+    @Test
+    public void select_a_product(){
+        productsPage.selectProduct(1);
+        Assertions.assertTrue(productDetailPage.isOnProductDetailPage(), "Not on product detail page!");
+    }
+
+    @Test
+    public void add_product_to_cart(){
+        productDetailPage.addToCart();
+        Assertions.assertTrue(homePage.isProductCountUp(), "Product count did not increase!");
+    }
+
+    @Test
+    public void go_to_cart(){
+        homePage.goToCart();
+        Assertions.assertTrue(cartPage.checkIfProductAdded(), "Product was not addes to card!");
+    }
 }
